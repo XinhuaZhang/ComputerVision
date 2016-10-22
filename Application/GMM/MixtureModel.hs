cb848e4e492487df9286f6495015518c19ee62a4
Xinhua Zhang <fantasyfrog@gmail.com>
2016-10-21 23:10:58 -0600

    deleted:    Application/GMM/GMM.hs 	new file:   Application/GMM/Gaussian.hs 	new file:   Application/GMM/MixtureModel.hs

---
+++ Application/GMM/MixtureModel.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
+module Application.GMM.MixtureModel where
+
+import           Application.GMM.Gaussian
+import           Data.Binary
+import           Data.Vector              as V
+import           GHC.Generics
+import           Prelude                  as P
+
+
+data MixtureModel a =
+  MixtureModel {numModel :: Int
+               ,model    :: V.Vector (Double,a)}
+  deriving (Show,Generic)
+
+instance (Binary a) =>
+         Binary (MixtureModel a) where
+  put (MixtureModel numModel' xs) =
+    do put numModel'
+       put $ V.toList xs
+  get =
+    do numModel' <- get
+       xs <- get
+       return (MixtureModel numModel'
+                            (V.fromList xs))
+                            
+
