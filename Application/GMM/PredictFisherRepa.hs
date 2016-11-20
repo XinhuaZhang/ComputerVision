8571fbd3d22faa340295d5543c5e52c8ac00662e
Xinhua Zhang <fantasyfrog@gmail.com>
2016-11-20 15:33:52 -0700

    A version which can be compiled without error.

--- Application/GMM/PredictFisherRepa.hs
+++ Application/GMM/PredictFisherRepa.hs
@@ -28,40 +28,6 @@ import           Foreign.Ptr
 import           Prelude                            as P
 import           System.Environment
 
-trainSink
-  :: ParallelParams -> FilePath -> TrainParams -> Bool -> Sink (VU.Vector Double) IO ()
-trainSink parallelParams filePath trainParams findCFlag =
-  do xs <- consume
-     if ((VU.length . P.head $ xs) /= (trainFeatureIndexMax trainParams))
-        then error $
-             "Number of feature in trainParams is not correct. (" P.++
-             (show . VU.length . P.head $ xs) P.++
-             " vs " P.++
-             (show $ trainFeatureIndexMax trainParams) P.++
-             ")"
-        else return ()
-     featurePtrs <-
-       xs `pseq` liftIO $ MP.mapM (getFeatureVecPtr . Dense . VU.toList) xs
-     label <- liftIO $ readLabelFile filePath
-     if findCFlag
-        then liftIO $ findParameterC trainParams label featurePtrs
-        else liftIO $ train trainParams label featurePtrs -- go label []
-  where go :: [Double]
-           -> [[Ptr C'feature_node]]
-           -> Sink (VU.Vector Double) IO ()
-        go label pss =
-          do xs <- CL.take (Parallel.batchSize parallelParams)
-             if P.length xs > 0
-                then do ps <-
-                          liftIO $
-                          P.mapM (getFeatureVecPtr . Dense . VU.toList) xs
-                        time <- liftIO getZonedTime
-                        liftIO $
-                          print . localTimeOfDay . zonedTimeToLocalTime $ time
-                        go label $! (ps : pss)
-                else liftIO $
-                     train trainParams label (P.concat . L.reverse $ pss)
-
 main =
   do args <- getArgs
      if P.null args
@@ -84,17 +50,6 @@ main =
                                          S.fromDistinctAscList
                                            [0 .. (freq params - 1)]
                                       ,getName = Pinwheels}
-         trainParams =
-           TrainParams {trainSolver = L2R_L2LOSS_SVC_DUAL
-                       ,trainC = c params
-                       ,trainNumExamples = P.length . lines $ imageList
-                       ,trainFeatureIndexMax =
-                          if isComplex params
-                             then (4 * getFilterNum filterParams) *
-                                  (numModel gmm)
-                             else (2 * getFilterNum filterParams) *
-                                  (numModel gmm)
-                       ,trainModel = modelName params}
          filters =
            makeFilter filterParams :: PolarSeparableFilter (R.Array U DIM3 (C.Complex Double))
      print params
@@ -109,8 +64,9 @@ main =
                              toUnboxed . computeS $
                              R.slice arr (Z :. All :. a :. b)) $
                     [(i,j)|i <- [0 .. ny - 1],j <- [0 .. nx - 1]]) =$=
-       (fisherVectorConduit parallelParams gmm) $$
-       trainSink parallelParams
-                 (labelFile params)
-                 trainParams
-                 (findC params)
+       (fisherVectorConduit parallelParams gmm) =$=
+       CL.mapM (getFeatureVecPtr . Dense . VU.toList) =$=
+       mergeSource (labelSource $ labelFile params) $$
+       predict (modelName params)
+               ((modelName params) P.++ ".out")
+       
