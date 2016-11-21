09eead14922324a9f7c3392b4cd0f9d8a325036d
Xinhua Zhang <xinhua@unm.edu>
2016-11-21 13:50:46 -0700

    Image module is for ploting; Labeledarray module is for storing as a binary

---
+++ CV/Array/Image.hs
@@ -0,0 +1,69 @@
+module CV.Array.Image where
+
+import           Codec.Picture
+import           Data.Array.Repa as R
+import           Data.Image
+import           Prelude         as P
+
+
+grayImage2Array
+  :: GrayImage -> Array U DIM3 Double
+grayImage2Array img = fromListUnboxed (Z :. 1 :. ny :. nx) $ pixelList img
+  where (ny,nx) = dimensions img
+
+colorImage2Array
+  :: ColorImage -> Array U DIM3 Double
+colorImage2Array img =
+  fromListUnboxed (Z :. 3 :. ny :. nx) $
+  P.concatMap pixelList
+              [r,g,b]
+  where (ny,nx) = dimensions img
+        (r,g,b) = colorImageToRGB img
+
+normalizeImage
+  :: Double -> Array U DIM3 Double -> Array U DIM3 Double
+normalizeImage upperBound img =
+  computeS $ R.map (\x -> (x - minV) / (maxV - minV) * upperBound) img
+  where maxV = foldAllS max 0 img
+        minV = foldAllS min 10000 img
+
+plotImage :: FilePath -> Array U DIM3 Double -> IO ()
+plotImage filePath img =
+  do let Z :. nyp' :. nxp' :. nfp' = extent img
+         normalizedImg =
+           normalizeImage (P.fromIntegral (maxBound :: Pixel8))
+                          img
+         w =
+           case nfp' of
+             1 ->
+               ImageY8 $
+               generateImage
+                 (\j i ->
+                    let v =
+                          fromIntegral . round $
+                          normalizedImg ! (Z :. 0 :. j :. i)
+                    in v)
+                 nyp'
+                 nxp'
+             3 ->
+               ImageRGB8 $
+               generateImage
+                 (\j i ->
+                    let r =
+                          fromIntegral . round $
+                          normalizedImg ! (Z :. 0 :. j :. i)
+                        g =
+                          fromIntegral . round $
+                          normalizedImg ! (Z :. 1 :. j :. i)
+                        b =
+                          fromIntegral . round $
+                          normalizedImg ! (Z :. 2 :. j :. i)
+                    in PixelRGB8 r g b)
+                 nyp'
+                 nxp'
+             _ ->
+               error $
+               "Image is neither a gray image nor a color image. There are " P.++
+               show nfp' P.++
+               " channels."
+     savePngImage filePath w
---
+++ CV/Array/LabeledArray.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DeriveGeneric #-}
+module CV.Array.LabeledArray where
+
+import           Data.Array.Repa     as R
+import           Data.Binary
+import           Data.Vector.Unboxed as VU
+import           GHC.Generics
+
+data LabeledArray sh e =
+  LabeledArray !Int
+               (Array U sh e)
+  deriving (Show,Generic)
+
+instance (Binary e
+         ,Unbox e
+         ,Shape sh) =>
+         Binary (LabeledArray sh e) where
+  put (LabeledArray label arr) =
+    do put label
+       put . listOfShape . extent $ arr
+       put . R.toList $ arr
+  get =
+    do label' <- get
+       shList <- get
+       elemList <- get
+       return $!
+         LabeledArray
+           label'
+           (fromListUnboxed (shapeOfList shList)
+                            elemList)
