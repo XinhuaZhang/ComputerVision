777652732edfbb14497b1c73007ea9e7f44fd9ad
Xinhua Zhang <xinhua@unm.edu>
2016-11-21 19:21:21 -0700

    Added binary version of rotate image dataset.

--- Application/RotateDataset/MainBinary.hs
+++ Application/RotateDataset/MainBinary.hs
@@ -1,27 +1,70 @@
 module Main where
 
 import           Application.RotateDataset.RotationRepa
+import           Control.Monad.IO.Class                 (liftIO)
 import           CV.Array.Image
+import           CV.Array.LabeledArray
+import           CV.IO.ImageIO
 import           CV.Utility.RepaArrayUtility            as RAU
 import           Data.Array.Repa                        as R
+import           Data.Conduit                           as C
+import           Data.Conduit.List                      as CL
 import           Data.Image
+import           Prelude                                as P
+import           System.Directory
+import           System.Environment
 
-main =
-  do img <-
-       readImage "/home/xinhua/Workspace/ComputerVision/Application/RotateDataset/image_0001.pgm"
-     let arr = grayImage2Array img
-         (Z :. nf :. ny :. nx) = extent arr
-         m = max ny nx
-         paddImg = RAU.pad [m,m,nf] arr
-         result =
-           recaleAndRotate2DImageS 256
-                                   36
-                                   (slice arr (Z :. (0 :: Int) :. All :. All))
-     -- plotImage "/Users/xzhang/WorkSpace/ComputerVision/Application/RotateDataset/test.png" (computeUnboxedS paddImg)
-     plotImage "/home/xinhua/Workspace/ComputerVision/Application/RotateDataset/test.png"
-               (computeUnboxedS . extend (Z :. (1 :: Int) :. All :. All) $
-                result) 
-  
-  
-  
-  
+readLabelFile :: FilePath -> IO [Int]
+readLabelFile filePath =
+  do bs <- readFile filePath
+     return . P.map (\x -> read x :: Int) . lines $! bs
+
+labelSource :: FilePath -> C.Source IO Int
+labelSource filePath =
+  do labels <- liftIO . readLabelFile $ filePath
+     sourceList labels
+
+main = do
+  (trainPath:trainLabelPath:testPath:testLabelPath:outputPath:isColor:_) <-
+    getArgs
+  putStrLn $ "trainPath: " P.++ trainPath
+  putStrLn $ "trainLabelPath: " P.++ trainLabelPath
+  putStrLn $ "testPath: " P.++ testPath
+  putStrLn $ "testLabelPath: " P.++ testLabelPath
+  putStrLn $ "outputPath: " P.++ outputPath
+  putStrLn $ "isColor: " P.++ isColor
+  let str =
+        if read isColor :: Bool
+          then "Color"
+          else "Gray"
+      n = 256
+      deg = 36
+      graySource path labelPath =
+        imagePathSource path =$= grayImageConduit =$= CL.map grayImage2Array =$=
+        mergeSource (labelSource labelPath) =$=
+        CL.map (\(label, img) -> LabeledArray label img)
+      colorSource path labelPath =
+        imagePathSource path =$= colorImageConduit =$= CL.map colorImage2Array =$=
+        mergeSource (labelSource labelPath) =$=
+        CL.map (\(label, img) -> LabeledArray label img)
+  P.mapM_
+    (\(x, y) ->
+       createDirectoryIfMissing
+         True
+         (outputPath P.++ "/" P.++ x P.++ "/" P.++ y P.++ "/" P.++ str)) $
+    (,) <$> ["Train", "Test"] <*> ["Original", "Rotated"]
+  if read isColor :: Bool
+    then do
+      colorSource trainPath trainLabelPath $$
+        writeLabeledImageSink
+          (outputPath P.++ "/Train/Original/" P.++ str P.++ ".bin")
+      colorSource trainPath trainLabelPath $$ rotateLabeledImageConduit n deg =$=
+        writeLabeledImageSink
+          (outputPath P.++ "/Train/Rotated/" P.++ str P.++ ".bin")
+    else do
+      graySource trainPath trainLabelPath $$
+        writeLabeledImageSink
+          (outputPath P.++ "/Train/Original/" P.++ str P.++ ".bin")
+      graySource trainPath trainLabelPath $$ rotateLabeledImageConduit n deg =$=
+        writeLabeledImageSink
+          (outputPath P.++ "/Train/Rotated/" P.++ str P.++ ".bin")
--- Application/RotateDataset/RotationRepa.hs
+++ Application/RotateDataset/RotationRepa.hs
@@ -1,98 +1,109 @@
 {-# LANGUAGE FlexibleContexts #-}
-module  Application.RotateDataset.RotationRepa where
+
+module Application.RotateDataset.RotationRepa where
 
 import           Control.Monad               as M
 import           Control.Monad.IO.Class
 import           CV.Array.LabeledArray
-
 import           CV.Utility.Coordinates
 import           CV.Utility.Parallel
 import           CV.Utility.RepaArrayUtility
 import           Data.Array.Repa             as R
+import           Data.Binary
 import           Data.Conduit
-import Data.Conduit.List           as CL
+import           Data.Conduit.List           as CL
 import           Data.List                   as L
 import           Data.Vector                 as V
 import           Data.Vector.Unboxed         as VU
 import           Prelude                     as P
 import           System.IO
 
-
 -- First pading image to be a square image then rotating it
-recaleAndRotate2DImageS :: (R.Source s Double)
-                        => Int
-                        -> [Double]
-                        -> Array s DIM2 Double
-                        -> [Array U DIM2 Double]
+recaleAndRotate2DImageS
+  :: (R.Source s Double)
+  => Int -> [Double] -> Array s DIM2 Double -> [Array U DIM2 Double]
 recaleAndRotate2DImageS n degs arr =
-  L.map (\deg ->
-           computeS $
-           fromFunction
-             (Z :. n :. n)
-             (\(Z :. j :. i) ->
-                let (j',i') =
-                      rotatePixel
-                        (VU.fromListN 4 $
-                         P.map (\f -> f (deg2Rad deg))
-                               [cos,sin,\x -> -(sin x),cos])
-                        (center,center)
-                        (fromIntegral j,fromIntegral i)
-                in if inRange j' && inRange i'
-                      then bicubicInterpolation
-                             ds
-                             ((j' - boundaryWith) * ratio
-                             ,(i' - boundaryWith) * ratio)
-                      else 0))
-        degs
-  where (Z :. ny :. nx) = extent arr
-        m = max ny nx
-        innerSize = floor (fromIntegral n / sqrt (2 :: Double))
-        boundaryWith = fromIntegral $ div (n - innerSize) 2
-        paddedImg = pad [m,m] arr
-        ds = computeDerivativeS (computeUnboxedS paddedImg)
-        center = fromIntegral n / 2
-        ratio = fromIntegral m / (fromIntegral innerSize - 1)
-        inRange :: Double -> Bool
-        inRange x =
-          x >= boundaryWith && x <= (boundaryWith + fromIntegral innerSize - 1)
-          
-rotatePixel :: VU.Vector Double
-            -> (Double,Double)
-            -> (Double,Double)
-            -> (Double,Double)
-rotatePixel mat (centerY,centerX) (y,x) = (y3,x3)
-  where x1 = x - centerX
-        y1 = y - centerY
-        (y2,x2) =
-          vecMatMult (y1,x1)
-                     mat
-        x3 = x2 + centerX
-        y3 = y2 + centerY
+  L.map
+    (\deg ->
+       computeS $
+       fromFunction
+         (Z :. n :. n)
+         (\(Z :. j :. i) ->
+            let (j', i') =
+                  rotatePixel
+                    (VU.fromListN 4 $
+                     P.map
+                       (\f -> f (deg2Rad deg))
+                       [cos, sin, \x -> -(sin x), cos])
+                    (center, center)
+                    (fromIntegral j, fromIntegral i)
+            in if inRange j' && inRange i'
+                 then bicubicInterpolation
+                        ds
+                        ( (j' - boundaryWith) * ratio
+                        , (i' - boundaryWith) * ratio)
+                 else 0))
+    degs
+  where
+    (Z :. ny :. nx) = extent arr
+    m = max ny nx
+    theta = atan (fromIntegral nx / fromIntegral ny)
+    xx = max (cos theta) (sin theta)
+    innerSize = floor (fromIntegral n * xx)
+    --innerSize = floor (fromIntegral n / sqrt (2 :: Double))
+    boundaryWith = fromIntegral $ div (n - innerSize) 2
+    paddedImg = pad [m, m] arr
+    ds = computeDerivativeS (computeUnboxedS paddedImg)
+    center = fromIntegral n / 2
+    ratio = fromIntegral m / (fromIntegral innerSize - 1)
+    inRange :: Double -> Bool
+    inRange x =
+      x >= boundaryWith && x <= (boundaryWith + fromIntegral innerSize - 1)
 
-vecMatMult
-  :: (Double,Double) -> VU.Vector Double -> (Double,Double)
-vecMatMult (x,y) vec = (a * x + c * y,b * x + d * y)
-  where a = vec VU.! 0
-        b = vec VU.! 1
-        c = vec VU.! 2
-        d = vec VU.! 3
-        
+rotatePixel :: VU.Vector Double
+            -> (Double, Double)
+            -> (Double, Double)
+            -> (Double, Double)
+rotatePixel mat (centerY, centerX) (y, x) = (y3, x3)
+  where
+    x1 = x - centerX
+    y1 = y - centerY
+    (y2, x2) = vecMatMult (y1, x1) mat
+    x3 = x2 + centerX
+    y3 = y2 + centerY
+
+vecMatMult :: (Double, Double) -> VU.Vector Double -> (Double, Double)
+vecMatMult (x, y) vec = (a * x + c * y, b * x + d * y)
+  where
+    a = vec VU.! 0
+    b = vec VU.! 1
+    c = vec VU.! 2
+    d = vec VU.! 3
 
 rotateLabeledImageConduit
-  :: Double -> Conduit (LabeledArray DIM3 Double) IO (LabeledArray DIM3 Double)
-rotateLabeledImageConduit deg =
+  :: Int
+  -> Double
+  -> Conduit (LabeledArray DIM3 Double) IO (LabeledArray DIM3 Double)
+rotateLabeledImageConduit n deg =
   awaitForever
     (\(LabeledArray label arr) ->
        let (Z :. nf :. _ny :. _nx) = extent arr
        in sourceList .
-          L.map (LabeledArray label .
-                 fromUnboxed (Z :. nf :. n :. n) .
-                 VU.concat . L.map R.toUnboxed) .
+          L.map
+            (LabeledArray label .
+             fromUnboxed (Z :. nf :. n :. n) . VU.concat . L.map R.toUnboxed) .
           L.transpose .
-          L.map (\i ->
-                   recaleAndRotate2DImageS n degs $
-                   R.slice arr (Z :. i :. All :. All)) $
+          L.map
+            (\i ->
+               recaleAndRotate2DImageS n degs $
+               R.slice arr (Z :. i :. All :. All)) $
           [0 .. nf - 1])
-  where len = round (360 / deg)
-        degs = L.map (* deg) [0 .. fromIntegral len - 1]
-        n = 256
+  where
+    len = round (360 / deg)
+    degs = L.map (* deg) [0 .. fromIntegral len - 1]
+
+
+writeLabeledImageSink :: FilePath -> Sink (LabeledArray DIM3 Double) IO ()
+writeLabeledImageSink filePath = do
+  xs <- CL.consume
+  liftIO $ encodeFile filePath xs
