3d1ef705cc1e470a1bc7f3c130a6dda7211bf27d
XinhuaZhang <fantasyfrog@gmail.com>
2016-12-23 16:45:55 -0700

    Finished the plot module

--- Application/RingFilter/Main.hs
+++ Application/RingFilter/Main.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import           Application.RotateDataset.RotationRepa
 import           Control.Monad                          as M
+import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Resource
 import           CV.Array.LabeledArray
 import           CV.Feature.PolarSeparableRepa
 import           CV.Filter.PolarSeparableFilter
+import           CV.IO.ImageIO
 import           CV.Utility.Parallel
 import           Data.Array.Repa                        as R
-import           Data.Conduit
+import           Data.Conduit                           as C
 import           Data.Conduit.List                      as CL
 import           Data.List                              as L
 import           Data.Set                               as S
@@ -17,9 +20,19 @@ import           Graphics.Rendering.Chart.Backend.Cairo
 import           Graphics.Rendering.Chart.Easy
 import           System.Environment
 
+readLabelFile :: FilePath -> IO [Int]
+readLabelFile filePath =
+  do bs <- readFile filePath
+     return . L.map (\x -> read x :: Int) . lines $! bs
+
+labelSource :: FilePath -> C.Source IO Int
+labelSource filePath =
+  do labels <- liftIO . readLabelFile $ filePath
+     sourceList labels
+
 
 main = do
-  (inputPath:_) <- getArgs
+  (inputPath:labelPath:_) <- getArgs
   let parallelParams =
         ParallelParams
         { numThread = 4
@@ -30,18 +43,24 @@ main = do
         { getSizeSet = (0, 0)
         , getDownsampleFactorSet = 1
         , getScaleSet = S.fromDistinctAscList [6]
-        , getRadialFreqSet = S.fromDistinctAscList [0 .. (8 - 1)]
-        , getAngularFreqSet = S.fromDistinctAscList [0 .. (8 - 1)]
+        , getRadialFreqSet = S.fromDistinctAscList [0 .. (4 - 1)]
+        , getAngularFreqSet = S.fromDistinctAscList [0 .. (4 - 1)]
         , getNameSet = Pinwheels
         }
-      deg = 36
-  (img@(LabeledArray _ arr):_) <- readLabeledImageBinary inputPath 1
+      deg = 36 :: Int
+  img <-
+    imagePathSource inputPath $$ readImageConduit False =$=
+    mergeSource (labelSource labelPath) =$=
+    CL.map (\(label, img) -> LabeledArray label $ computeUnboxedS img) =$=
+    CL.take 1
   rotatedLabeledImg <-
-    CL.sourceList [img] $$
-    rotateLabeledImageConduit parallelParams (fromIntegral deg) =$=
+    CL.sourceList img $$
+    rescaleRotateLabeledImageConduit parallelParams 255 (fromIntegral deg) =$=
     CL.consume
-  let (Z :. _ :. nRows :. numCols) = extent arr
-      centerIdx = (div nRows 2) * numCols + (div numCols 2)
+  let !ex@(Z :. _ :. nRows :. numCols) =
+        extent $ (\(LabeledArray _ arr) -> arr) $ L.head $ rotatedLabeledImg
+      !centerIdx = (div nRows 2) * numCols + (div numCols 2)
+  print ex
   mag <-
     runResourceT $
     (CL.sourceList rotatedLabeledImg $$
@@ -50,12 +69,18 @@ main = do
        [filterParamsSet1]
        1 =$=
      CL.consume)
-  let centerMag =
+  let !centerMag =
         L.transpose $ L.map (\(_, vecs) -> L.map (VU.! centerIdx) vecs) mag
-  toFile def "hehe.png" $
+      !normlizedCenterMag = L.map (\x -> L.map (/ L.maximum x) x) centerMag
+  toFile def "original.png" $
     do layout_title .= "Magnitude"
-       plot
-         (line
-            ""
-            (L.map (\xs -> L.zip [0 :: Int,deg .. (360 - deg)] xs) centerMag))
--- M.zipWithM_ (\xs i -> plot $ line (show i) [[(j, xs) | j <- [0,deg..(360 - deg)]]]) centerMag [1..]
+       M.zipWithM_
+         (\xs i -> plot $ line (show i) [L.zip [0,deg .. (360 - deg)] xs])
+         centerMag
+         [1 ..]
+  toFile def "normalized.png" $
+    do layout_title .= "Magnitude"
+       M.zipWithM_
+         (\xs i -> plot $ line (show i) [L.zip [0,deg .. (360 - deg)] xs])
+         normlizedCenterMag
+         [1 ..]
